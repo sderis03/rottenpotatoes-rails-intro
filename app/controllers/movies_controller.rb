548d4628755c1161f5af5f050eab98f5e29d6e58
Sarah Deris <sderis@citadel.edu>
2017-09-27 13:58:36 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,7 +24,13 @@ class MoviesController < ApplicationController
       @date_header='hilite'
     end
     
-    @movies = Movie.order(ordering)
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || {}
+    
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map{|rating| [rating, rating ]}]
+    end
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
 
   def new
